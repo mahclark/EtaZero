ddd590197c25860539debb4f2f275064dc48e4b4
mahclark <mc2124@cam.ac.uk>
2020-12-19 00:54:22 +0000

    performed proof-of-concept test on GNN

--- trainer.py
+++ trainer.py
@@ -1,3 +1,4 @@
+import csv
 import datetime
 import numpy as np
 import os
@@ -5,9 +6,10 @@ import torch
 from torch import nn
 from tqdm import tqdm
 from networks.dgl_value_win_network import DGLValueWinNetwork
-from sevn import Game
+from sevn import Game, State
 from agents.random_agent import RandomAgent
 from agents.eta_zero import EtaZero
+from agents.uct_agent import UCTAgent
 
 def takable_label_fn(g, state):
     """
@@ -34,26 +36,15 @@ class Trainer:
         self.model = model
         self.prev_model_path = None
 
-    
-    def train(
-        self,
-        loss_fn=nn.MSELoss(reduction='sum'),
-        n_epochs=10,
-        lr=0.001, # learning rate
-        l2norm=10e-4, # L2 norm coefficient
-        batch_size=32,
-        num_games=640,
-        game_base=5):
-
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=lr, weight_decay=l2norm)
-
+    def _default_data_generator(self, num_games=50, game_base=7):
         data = []
         labels = []
 
         print("generating data from {} games...".format(num_games))
         for _ in tqdm(range(num_games)):
             game = Game(game_base)
-            eta_zero = EtaZero(game, self.model, training=True, samples_per_move=20)
+            eta_zero = EtaZero(self.model, training=True, samples_per_move=20)
+            eta_zero.set_game(game)
             
             while not game.over():
                 game.make_move(eta_zero.select_move())
@@ -62,19 +53,50 @@ class Trainer:
 
             data += data_x
             labels += data_y
+        
+        return data, labels
+    
+    def train(
+        self,
+        loss_fn=nn.MSELoss(reduction='sum'),
+        n_epochs=10,
+        lr=0.001, # learning rate
+        l2norm=10e-4, # L2 norm coefficient
+        batch_size=32,
+        data_fn=None,
+        history_path=None):
+
+        if data_fn == None:
+            data_fn = self._default_data_generator
+
+        all_data = data_fn()
+
+        if len(all_data) == 2:
+            X_train, y_train = all_data
+            X_val, y_val = None, None
+
+        elif len(all_data) == 4:
+            X_train, y_train, X_val, y_val = all_data
+
+        else:
+            raise Exception("Data function produced unexpected number of data series ({}).".format(len(all_data))) 
+
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=lr, weight_decay=l2norm)
+
+        history = []
 
         print("start training...")
         for epoch in range(n_epochs):
             epoch_loss = 0
             self.model.train()
 
-            batch_count = int(len(data) / batch_size)
+            batch_count = int(len(X_train) / batch_size)
             for _ in range(batch_count):
-                sample_indexes = np.random.randint(len(data), size=batch_size)
+                sample_indexes = np.random.randint(len(X_train), size=batch_size)
                 
                 for index in sample_indexes:
-                    g = data[index]
-                    label = labels[index]
+                    g = X_train[index]
+                    label = y_train[index]
                     
                     output = self.model.forward(g)
                     
@@ -86,10 +108,25 @@ class Trainer:
                     optimizer.step()
                     epoch_loss += loss.detach().item()
                 
-            epoch_loss /= len(data)
+            epoch_loss /= len(X_train)
 
-            print("Epoch {:05d} | ".format(epoch) +
-                "Loss: {:.4f} | ".format(epoch_loss))
+            report = "Epoch {:05d} | ".format(epoch) + \
+                "Loss: {:.4f} | ".format(epoch_loss)
+            
+            if X_val != None:
+                correct = 0
+                for x, y in zip(X_val, y_val):
+                    correct += self.model.forward(x)[0]*y[0] > 0
+                report += "Val Acc: {:.3f}".format(correct/len(X_val))
+
+                history.append((epoch, "{:.4f}".format(epoch_loss), "{:.3f}".format(correct/len(X_val))))
+
+            print(report)
+        
+        if history_path != None:
+            writer = csv.DictWriter(open(history_path, 'a', newline=''), fieldnames=["Epoch","MSELoss","ValAcc"])
+            for epoch, loss, acc in history:
+                writer.writerow({"Epoch":epoch, "MSELoss":loss, "ValAcc":acc})
         
         # if self.prev_model_path != None:
         #     prev_model = torch.load(self.prev_model_path)
@@ -110,60 +147,77 @@ class Trainer:
             datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".pt"
         )
     
-    def compare(self, model, num_games=4, game_base=5):
+    @staticmethod
+    def generate_uct_win_data(path, base=5):
 
-        for i in range(num_games):
-            game = Game(game_base)
+        game = Game(base)
 
-            if i % 2 == 0:
-                p1 = EtaZero(game, self.model, num=1)
-                p2 = EtaZero(game, model, num=2)
-            else:
-                p1 = EtaZero(game, model, num=1)
-                p2 = EtaZero(game, self.model, num=2)
+        uct1 = UCTAgent(max_evals_per_turn=500)
+        uct2 = UCTAgent(max_evals_per_turn=500)
+        
+        uct1.set_game(game)
+        uct2.set_game(game)
 
-            next_player = p1
-            while not game.over():
-                game.make_move(next_player.select_move())
-                next_player = p1 if next_player == p2 else p2
-            
-            if (i % 2 == 0) == (game.state.outcome == 1):
-                print("Training model won as player {}".format(i % 2 + 1))
-            else:
-                print("Training model lost as player {}".format(i % 2 + 1))
+        p = uct1
+
+        states = [game.state]
+
+        while not game.over():
+            game.make_move(p.select_move())
+            states.append(game.state)
+
+            p = uct1 if p == uct2 else uct2
+        
+        value = game.state.outcome
+        
+        writer = csv.writer(open(path, 'a', newline=''))
+        for state in states:
+            writer.writerow([state, value])
+            value *= -1
 
 if __name__ == "__main__":
+    path = "uct_win_data/data.csv"
+    
+    # for i in range(100):
+    #     Trainer.generate_uct_win_data(path, base=5)
+    
+    def get_win_data():
+        with open(path) as data_file:
+            reader = csv.reader(data_file)
 
-    trainer = Trainer(load_path="models\\2020-11-28-12-01-33.pt")
-    while True:
-        # num_games = 20 if i == 0 else 100
-        trainer.train(num_games=100, game_base=5, batch_size=8, n_epochs=20)
-
-    # prove generality by training on 5x5 board and testing on 9x9 board
-    # game = Game(9)
-    # agent = RandomAgent(game)
-
-    # while not game.over():
-    #     g = game.state.to_dgl_graph()
-    #     h = trainer.model.forward(g)
-
-    #     print()
-    #     for row in range(game.base):
-    #         for col in range(game.base):
-    #             tile = game.state.board.board[row][col]
-    #             if tile == -1:
-    #                 print("- ",end="")
-    #             elif (row, col) in game.state.board.get_takable():
-    #                 print("O ",end="")
-    #             else:
-    #                 print("X ",end="")
-    #         print()
+            data = []
+            labels = []
+            for row in reader:
+                state = State.from_str(row[0])
+                if state.outcome == 0 and sum(state.board.get_at(x,y) > -1 for x in range(5) for y in range(5)) < 10:
+                    data.append(state.to_dgl_graph())
+                    labels.append(torch.tensor([float(row[1])]))
         
-    #     chosen = []
-    #     for score, pos in zip(h, g.ndata["position"]):
-    #         if score[0] > .5:
-    #             chosen.append((int(pos[0]),int(pos[1])))
-        
-    #     print(chosen)
+        print(len(data))
 
-    #     game.make_move(agent.select_move())
+        np.random.seed(42)
+        idxs = np.random.choice(np.arange(0, len(data)), replace=False, size=int(len(data)*.8))
+        idxs.sort()
+        val_idxs = []
+        j = 0
+        for i in range(len(data)):
+            while j < len(idxs)-1 and idxs[j] < i:
+                j += 1
+            
+            if idxs[j] != i:
+                val_idxs.append(i)
+        val_idxs = np.array(val_idxs)
+
+        def get(v, i):
+            return [v[j] for j in i]
+
+        x = get(data, idxs)
+        y = get(labels, idxs)
+        val = get(data, val_idxs)
+        val_y = get(labels, val_idxs)
+        
+        return x, y, val, val_y
+    
+    model = DGLValueWinNetwork(dims=[3,10,10,1])
+    trainer = Trainer(model=model)#, load_path="models/2020-12-18-23-06-15.pt")
+    trainer.train(data_fn=get_win_data, n_epochs=20, history_path="uct_win_data/train_history.csv")
