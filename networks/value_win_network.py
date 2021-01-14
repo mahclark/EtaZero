fb2efcf51cea342d23d06b61a4c5886c1496434b
mahclark <mc2124@cam.ac.uk>
2021-01-14 17:23:50 +0000

    updated definition for val win network

--- networks/value_win_network.py
+++ networks/value_win_network.py
@@ -4,8 +4,12 @@ class ValueWinNetwork(Network):
     
     def evaluate(self, state):
         """
-        Given a game state, returns:
-            - v, the predicted win probabilityfor the next player
-            - w, the predicted winner of the game
+        Given a game state, returns a tuple/tensor with:
+            - v, the predicted win probability for the other player in range [-1, 1]
+                - where 0.9 implies the current player has 5% chance of winning
+                - and -0.1 implies the current player has 55% chance of winning
+            - w, the predicted winner of the game in range [-1, 1]
+                - where 1 implies the current player is predicted to win
+                - where -1 implies the current player is predicted to lose
         """
         raise Exception("Function evaluate() not implemented!")
