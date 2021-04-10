1b231fbdc792e01089626884ab321602fc1a3068
mahclark <mc2124@cam.ac.uk>
2021-04-10 23:00:23 +0100

    fixed raw agent to work for policy-value nets

--- src/agents/network_agent.py
+++ src/agents/network_agent.py
@@ -1,7 +1,11 @@
 from agents.agent import Agent
+import torch
 
 
 class RawNetwork(Agent):
+    """
+    Agent which makes moves purely from guidance from a policy-value network
+    """
 
     name = "Raw Network"
 
@@ -9,24 +13,9 @@ class RawNetwork(Agent):
         self.network = network
         self.elo_id = f"Raw-{self.network.elo_id}"
 
-    def select_move(self):
+    def select_move(self):        
+        policy, value = self.network.evaluate(self.game.state)
 
-        best = None
-        best_score = -float("inf")
-        for move in self.game.get_moves():
-            self.game.make_move(move)
+        self.set_confidence((value + 1)/2)
 
-            if self.game.over():  # and self.game.state.board.is_empty():
-                score = -self.game.state.outcome * self.game.state.next_go
-            else:
-                score, win = self.network.evaluate(self.game.state)
-
-            self.game.undo_move()
-
-            if score > best_score:
-                best = move
-                best_score = score
-
-        self.set_confidence((best_score + 1)/2)
-
-        return best
+        return self.game.get_moves()[torch.argmax(policy).tolist()]
