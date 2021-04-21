c368fd1c89c1678eb2ac204cba4f6b867e593106
Max Clark <mahclark@aol.com>
2021-04-21 01:26:04 +0100

    changed scripts.py to run python from poetry's virtual environment

--- scripts.py
+++ scripts.py
@@ -1,24 +1,25 @@
+import sys
 from subprocess import check_call
 
 
 def play():
-    check_call(["python", "-m", "game.play"], cwd="src")
+    check_call([sys.executable, "-m", "game.play"], cwd="src")
 
 
 def train():
-    check_call(["python", "-m", "networks.trainer"], cwd="src")
+    check_call([sys.executable, "-m", "networks.trainer"], cwd="src")
 
 
 def arena():
-    check_call(["python", "-m", "agent_evaluation.arena"], cwd="src")
+    check_call([sys.executable, "-m", "agent_evaluation.arena"], cwd="src")
 
 
 def timer():
-    check_call(["python", "-m", "agent_evaluation.agent_timer"], cwd="src")
+    check_call([sys.executable, "-m", "agent_evaluation.agent_timer"], cwd="src")
 
 
 def testnet():
-    check_call(["python", "-m", "agent_evaluation.network_tester"], cwd="src")
+    check_call([sys.executable, "-m", "agent_evaluation.network_tester"], cwd="src")
 
 def fmt():
-    check_call(["python", "-m", "black", "src"])
+    check_call([sys.executable, "-m", "black", "src"])
